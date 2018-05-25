21bffc77f9c6faf8fbc704aecd08739f0ce55e46
João Palma <joaowd@outlook.com>
2018-05-25 01:49:57 +0100

    Make login View a IRootView

--- LetterApp.iOS/Views/Login/LoginViewController.cs
+++ LetterApp.iOS/Views/Login/LoginViewController.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using LetterApp.Core.ViewModels;
+using LetterApp.iOS.Interfaces;
 using LetterApp.iOS.Views.Base;
 using UIKit;
 
 namespace LetterApp.iOS.Views.Login
 {
-    public partial class LoginViewController : XViewController<LoginViewModel>
+    public partial class LoginViewController : XViewController<LoginViewModel>, IRootView
     {
         public LoginViewController() : base("LoginViewController", null) {}
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
         }
     }
 }
