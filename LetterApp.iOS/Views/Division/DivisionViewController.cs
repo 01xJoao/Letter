cdc63561ecb83752da4bb848a1f5ceaf46f3b7ff
João Palma <joaowd@outlook.com>
2018-07-15 19:41:30 +0100

    Add Division VC

---
+++ LetterApp.iOS/Views/Division/DivisionViewController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LetterApp.Core.ViewModels;
+using LetterApp.iOS.Views.Base;
+using UIKit;
+
+namespace LetterApp.iOS.Views.Division
+{
+    public partial class DivisionViewController : XViewController<DivisionViewModel>
+    {
+        public DivisionViewController() : base("DivisionViewController", null) {}
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+    }
+}
+
