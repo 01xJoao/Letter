521af1ae73bf57125ef0547c1a8ddf8f15b3c127
João Palma <joaowd@outlook.com>
2018-09-10 21:54:52 +0100

    Working on MessagerService

--- LetterApp.Core/Services/Interfaces/IMessengerService.cs
+++ LetterApp.Core/Services/Interfaces/IMessengerService.cs
@@ -1,4 +1,9 @@
-﻿namespace LetterApp.Core.Services.Interfaces
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SendBird;
+
+namespace LetterApp.Core.Services.Interfaces
 {
     public interface IMessengerService
     {
@@ -6,5 +11,11 @@
         void ConnectMessenger();
         void DisconnectMessenger();
         void RegisterMessengerToken();
+
+        Task<GroupChannel> CreateChannel(List<string> users);
+        Task<List<GroupChannel>> GetChannels(List<string> users);
+        Task<List<GroupChannel>> GetAllChannels();
+        Task<GroupChannel> GetCurrentChannel(string userId);
+        Task<UserMessage> SendMessage(Tuple<GroupChannel, string> message);
     }
 }
--- LetterApp.iOS/Services/MessengerService.cs
+++ LetterApp.iOS/Services/MessengerService.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using LetterApp.Core;
 using LetterApp.Core.Services.Interfaces;
 using SendBird;
@@ -8,9 +11,20 @@ namespace LetterApp.iOS.Services
 {
     public class MessengerService : IMessengerService
     {
+        const string _handlerId = "SendBirdHandler";
+
         public void InitializeMessenger()
         {
+            SendBirdClient.ChannelHandler ch = new SendBirdClient.ChannelHandler();
+            SendBirdClient.ConnectionHandler cnh = new SendBirdClient.ConnectionHandler();
+
+            //Handlers
+
             SendBirdClient.Init("46497603-C6C5-4E64-9E05-DCCAF5ED66D1");
+            SendBirdClient.AddChannelHandler(_handlerId, ch);
+            SendBirdClient.AddConnectionHandler(_handlerId, cnh);
+
+            //TODO Remove handlers?
         }
 
         public void ConnectMessenger()
@@ -53,6 +67,91 @@ namespace LetterApp.iOS.Services
                 RegisterMessengerToken();
                 Connectivity.ConnectivityChanged -= Connectivity_ConnectivityChanged;
             }
+        }
+
+        public Task<GroupChannel> CreateChannel(List<string> users)
+        {
+            var tcs = new TaskCompletionSource<GroupChannel>();
+
+            GroupChannel.CreateChannelWithUserIds(users, true, (channel, e) => {
+                if (e != null)
+                    tcs.TrySetCanceled();
+
+                tcs.TrySetResult(channel);
+            });
+
+            return tcs.Task;
+        }
+
+        public Task<List<GroupChannel>> GetAllChannels()
+        {
+            var tcs = new TaskCompletionSource<List<GroupChannel>>();
+
+            GroupChannelListQuery mQuery = GroupChannel.CreateMyGroupChannelListQuery();
+
+            mQuery.Next((List<GroupChannel> list, SendBirdException e) => 
+            {
+                if (e != null)
+                    tcs.TrySetCanceled();
+
+                tcs.TrySetResult(list);
+            });
+
+            return tcs.Task;
+        }
+
+        public Task<List<GroupChannel>> GetChannels(List<string> users)
+        {
+            var tcs = new TaskCompletionSource<List<GroupChannel>>();
+
+            GroupChannelListQuery filteredQuery = GroupChannel.CreateMyGroupChannelListQuery();
+            filteredQuery.SetUserIdsIncludeFilter(users, GroupChannelListQuery.QueryType.OR);
+
+            filteredQuery.Next((List<GroupChannel> queryResult, SendBirdException e) => 
+            {
+                if (e != null)
+                    tcs.TrySetCanceled();
+
+                tcs.TrySetResult(queryResult);
+            });
+
+            return tcs.Task;
+        }
+
+        public Task<GroupChannel> GetCurrentChannel(string userId)
+        {
+            var tcs = new TaskCompletionSource<GroupChannel>();
+
+            GroupChannelListQuery filteredQuery = GroupChannel.CreateMyGroupChannelListQuery();
+            filteredQuery.SetUserIdsExactFilter(new List<string> { userId });
+
+            filteredQuery.Next((List<GroupChannel> queryResult, SendBirdException e) =>
+            {
+                if (e != null)
+                    tcs.TrySetCanceled();
+
+                tcs.TrySetResult(queryResult.FirstOrDefault());
+            });
+
+            return tcs.Task;
+        }
+
+        public Task<UserMessage> SendMessage(Tuple<GroupChannel,string> data)
+        {
+            var tcs = new TaskCompletionSource<UserMessage>();
+
+            var channel = data.Item1;
+            var message = data.Item2;
+
+            channel.SendUserMessage(message, (msg, e) =>
+            {
+                if (e != null)
+                    tcs.TrySetCanceled();
+                   
+                tcs.TrySetResult(msg);
+            });
+
+            return tcs.Task;
         }
     }
 }
