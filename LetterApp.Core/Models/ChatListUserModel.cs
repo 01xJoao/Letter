472f4b68f5c220b7f26760f2c53d51d6687683fb
João Palma <joaowd@outlook.com>
2018-09-22 14:55:29 +0100

    Create MessagesModel, add messagelist to chatlist

--- LetterApp.Core/Models/ChatListUserModel.cs
+++ LetterApp.Core/Models/ChatListUserModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LetterApp.Core.Models
 {
@@ -17,5 +18,6 @@ namespace LetterApp.Core.Models
         public int MemberPresence { get; set; }
         public long MemberPresenceConnectionDate { get; set; }
         public int UnreadMessagesCount { get; set; }
+        public IList<MessagesModel> MessagesList { get; }
     }
 }
---
+++ LetterApp.Core/Models/MessagesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Realms;
+
+namespace LetterApp.Core.Models
+{
+    public class MessagesModel : RealmObject
+    {
+        public int MessageSenderId { get; set; }
+        public string MessageData { get; set; }
+        public int MessageType { get; set; }
+        public long MessageDateTicks { get; set; }
+        public string CustomData { get; set; }
+    }
+}
